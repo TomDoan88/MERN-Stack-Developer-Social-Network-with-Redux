b155e2443966cf951ad68e5d5cd57daa9fc190b6
Tronic <thomasdoan88@gmail.com>
2019-09-25 21:52:09 +1000

    Created the User Model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+// We want this user model to contain these types of data
+// Name - Password - Avatar - Date created
+// Each section would have their own requirement.
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
