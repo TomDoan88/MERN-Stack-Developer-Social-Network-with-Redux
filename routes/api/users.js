16091a984a22b3ef697363b88226fa4b2bdea7bb
Tronic <thomasdoan88@gmail.com>
2019-09-26 23:04:27 +1000

    Added Validator for the input

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 // @route  POST  api/users
 // @desc   Register route
@@ -8,8 +9,27 @@ const router = express.Router();
 //   1st: because users profile need to be authenticated
 //2nd: You need a token to access a route, it needs to be authenticated
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("User Route");
-});
+router.post(
+  "/",
+  // Checking for all the validation
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Please include a password").isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const error = validationResult(req);
+    // If the inputted result is not empty, but has errors then
+    // This part is handling the response
+    // If users do not send the information correctly then it is a bad request
+    // We need to include a message that says so.
+    // console.log(req.body);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ error: error.array() });
+    }
+    res.send("User Route");
+  }
+);
 module.exports = router;
