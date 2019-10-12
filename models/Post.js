bc02a90ee456cc0fbe1d66a60fd26175eb0b8db7
Tronic <thomasdoan88@gmail.com>
2019-10-13 01:14:20 +1100

    Created Post Model for the database

---
+++ models/Post.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// We want post to be connected to a user
+// MUST reference to a user, pass in user Object
+// Result = user connect to Posts
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Scheme.Types.ObjectId,
+        ref: "user"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Scheme.Types.ObjectId,
+        ref: "user"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        // This date is for the user post submite date
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  // This date is for the post itself
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = new mongoose.model("post", PostSchema);
