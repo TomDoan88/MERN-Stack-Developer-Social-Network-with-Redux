ee62ae1956c1aa64931d7d9438ff0d9cb412813d
Tronic <thomasdoan88@gmail.com>
2020-04-25 02:25:53 +1000

    Updated mongoose dependencies to a new version

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -12,10 +12,8 @@ export const loadUser = () => async dispatch => {
   if (localStorage.token) {
     setAuthToken(localStorage.token);
   }
-
   try {
-    const res = axios.get("api/auth");
-
+    const res = await axios.get("api/auth");
     dispatch({
       type: USER_LOADED,
       payload: res.data
@@ -39,7 +37,7 @@ export const register = ({ name, email, password }) => async dispatch => {
   const body = JSON.stringify({ name, email, password });
 
   try {
-    const res = await axios.post("/api/users", body, config);
+    const res = await axios.post("api/users", body, config);
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data
